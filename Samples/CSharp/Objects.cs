2d48823f94302e83e2f6035cad6ab2be7e78d77a
Carla Vazquez <cvazquez@fsmb.org>
2018-07-10 16:40:31 -0500

    Fixed samples

---
+++ Samples/CSharp/Objects.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using System;
+
+namespace MedApiSample
+{
+    class LicenseSummary
+    {
+        public string Fid { get; set; }
+        public int Count { get; set; }
+
+        public override string ToString()
+        {
+            return "Fid: " + Fid + ", Count: " + Count;
+        }
+    }
+
+    class AccessToken
+    {
+        [JsonProperty("access_Token")]
+        public string Token { get; set; }
+
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+    }
+
+    public class MedicalProfessionalIdentity
+    {
+        public string Fid { get; set; }
+    }
+
+    public class PdcPractitioner
+    {
+        public string Fid { get; set; }
+        public IEnumerable<BoardOrder> Orders { get; set; }
+        public IEnumerable<License> Licenses { get; set; }
+    }
+
+    public class BoardOrder
+    {
+        public string Category { get; set; }
+        public IEnumerable<CodedDescription> Actions { get; set; }
+    }
+
+    public class License
+    {
+        public string IssuerName { get; set; }
+        public string IssuerShortName { get; set; }
+        public string LicenseNumber { get; set; }
+    }
+
+    public class CodedDescription
+    {
+        [JsonProperty("actionCode")]
+        public string Code { get; set; }
+
+        [JsonProperty("actionDescription")]
+        public string Description { get; set; }
+    }
+}
--- Samples/CSharp/Program.cs
+++ Samples/CSharp/Program.cs
@@ -1,159 +1,37 @@
-using System;
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
-namespace CSharp
+namespace MedApiSample
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            string baseUrl, authUrl, clientID, clientSecret, scope;
-            Console.Write("Please enter the auth url: ");
-            authUrl = Console.ReadLine();
-            Console.Write("Please enter the base url: ");
-            baseUrl = Console.ReadLine();
-            Console.Write("Please enter the client id: ");
-            clientID = Console.ReadLine();
-            Console.Write("Please enter the client secret: ");
-            clientSecret = Console.ReadLine();
-            Console.Write("Please enter the authorization scope: ");
-            scope = Console.ReadLine();
+            var accessToken = AuthenticateUser("client", "password"); //replace with your clientId and clientSecret
+            var licenseService = new LicensureService(_medClient, accessToken);
+            var practitionerService = new PractitionerService(_medClient, accessToken);
 
-            var License = new LicensureClient( clientID, clientSecret,authUrl, scope, baseUrl);
-            var Practitioner = new PractitionerClient(clientID, clientSecret, authUrl, scope, baseUrl);
-            char choice = ' ';
-            string thing;
+            var licenseCount = licenseService.GetLicenseStatusAsync("205147010", CancellationToken.None);
+            licenseCount.Wait();
 
-            
-            do
-            {
-                try
-                {
-                    Console.Write("\n\nWhich resource do you want?\n1. Practitioners\n2. Licensure\n0. to exit\n\n");
-                    thing = Console.ReadLine();
-                    choice = thing[0];
+            Console.WriteLine("The results of GetLicenseStatusAsync using fid 205147010: {0}", licenseCount.Result.ToString());
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
 
-                    switch (choice)
-                    {
-                        case '0':
-                            Console.WriteLine("Exiting app...");
-                            break;
-                        case '1':
-                            PractitionerMenu(Practitioner);
-                            break;
-                        case '2':
-                            LicensureMenu(License);
-                            break;
-                        default:
-                            Console.WriteLine("Invalid input. Try again.");
-                            continue;
-                    }
-
-
-                }catch(Exception e) //most likely to fail due to invalid auth
-                {
-                    License.Authenticate();
-                    Practitioner.Authenticate();
-                }
-
-
-            } while (choice != '0');
-            
-            
         }
 
-        public static void LicensureMenu(LicensureClient license)
+        static string AuthenticateUser(string clientId, string clientSecret)
         {
-            char choice;
-            string fid, thing;
-            do
-            {
-                Console.Write("\n\nWhich Request do you want to make?\n1. GetLicenseStatus\n0. to exit\n\n");
-                thing = Console.ReadLine();
-                choice = thing[0];
-                switch (choice)
-                {
-                    case '0':
-                        break;
-                    case '1':
-                        Console.Write("Please enter the practitioner's fid: ");
-                        fid = Console.ReadLine();
-                        var response =  license.AsyncGetLicenseStatus(fid);
-                        response.Wait();
-                        Console.WriteLine(response.Result.Content.ReadAsStringAsync().Result.ToString());
-                        break;
-                    default:
-                        Console.WriteLine("Invalid input. Try again.");
-                        continue;
-                }
-            } while (choice != '0');
+            var authClient = new AuthenticationService(_authenticationClient);
+            var accessToken = authClient.GetAccessTokenAsync(clientId, clientSecret, CancellationToken.None).Result;
+            return accessToken.Token;
         }
 
-        public static void PractitionerMenu(PractitionerClient practitioner)
-        {
-            char choice;
-            string name, date, varies, fid, thing;
-            do
-            {
-                Console.Write("Which Request do you want to make?\n1. FindPractitionerBySSN\n2. FindPractitionerByLicense\n3. RetrieveProfile\n4. RetrieveBOandL\n0. to exit\n\n");
-                thing = Console.ReadLine();
-                choice = thing[0];
-                switch (choice)
-                {
-                    case '0':
-                        break;
-                    case '1':
-                    {
-                        Console.Write("Please enter the practitioner's full name: ");
-                        name = Console.ReadLine();
-                        Console.Write("Please enter the practitioner's date of birth in mm/dd/yyyy format: ");
-                        date = Console.ReadLine();
-                        Console.Write("Please enter the last four digits of the practitioner's SSN: ");
-                        varies = Console.ReadLine();
-                        var response = practitioner.FindPractitionerBySSN(name, date, varies);
-                        response.Wait();
-                        Console.WriteLine(response.Result.Content.ReadAsStringAsync().Result.ToString());
-                        break;
-                    }
-                    case '2':
-                    {
-                        Console.Write("Please enter the practitioner's full name: ");
-                        name = Console.ReadLine();
-                        Console.Write("Please enter the practitioner's date of birth in mm/dd/yyyy format: ");
-                        date = Console.ReadLine();
-                        Console.Write("Please enter the practitioner's license number: ");
-                        varies = Console.ReadLine();
-                        var response = practitioner.FindPractitionerBySSN(name, date, varies);
-                        response.Wait();
-                        Console.WriteLine(response.Result.Content.ReadAsStringAsync().Result.ToString());
-                        break;
-                    }                        
-                    case '3':
-                    {
-                        Console.Write("Please enter the practitioner's fid: ");
-                        fid = Console.ReadLine();
-                        var response = practitioner.RetrieveProfile(fid);
-                        response.Wait();
-                        Console.WriteLine(response.Result.Content.ReadAsStringAsync().Result.ToString());
-                        break;
-                    }
-                    case '4':
-                    {
-                        Console.Write("Please enter the practitioner's fid: ");
-                        fid = Console.ReadLine();
-                        var response = practitioner.RetrieveBOandL(fid);
-                        response.Wait();
-                        Console.WriteLine(response.Result.Content.ReadAsStringAsync().Result.ToString());
-                        break;
-                    }
-                    default:
-                        Console.WriteLine("Invalid input. Try again.");
-                        continue;
-                }
-            } while (choice != '0');
-
-        }
-
-
+        private static readonly HttpClient _medClient = new HttpClient() { BaseAddress = new Uri("https://tempuri.org") }; //replace with API address
+        private static readonly HttpClient _authenticationClient = new HttpClient() { BaseAddress = new Uri("https://tempuri.org") }; //replace with authentication address
     }
 }
